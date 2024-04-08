16bece2fefbfdf1181f03745f10f80eace8c55e5
Victor Espinoza-Guerra <thesilvervelvets@gmail.com>
2024-04-08 11:55:47 -0700

    rebuild

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
-#define SDL_MAIN_HANDLED
 
-#include <iostream>
-#include "application.hpp"
 #include <SDL2\SDL.h> // Using SDL and standard IO
-#include <stdio.h>
-
+#include <iostream>
 
 
 int main()
