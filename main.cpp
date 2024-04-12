0d18994ae88e03f9391d2dd77dc70c65be3d422c
Victor Espinoza-Guerra <thesilvervelvets@gmail.com>
2024-04-11 19:44:52 -0700

    Finalizing code...

--- main.cpp
+++ main.cpp
@@ -15,7 +15,23 @@ int main(int argc, char* args[])
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}
-
+	else
+	{
+		window = SDL_CreateWindow("SDL2 Initialized!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Event e; bool quit = false; while (quit == false) {
+				while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; }
+			}
+		}
+	}
 
 	return 0;
 }
