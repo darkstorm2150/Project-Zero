8da3f8a910e786b9c801a51b414476b409e46ff6
Victor Espinoza-Guerra <thesilvervelvets@gmail.com>
2024-04-15 09:39:04 -0700

    Activated SDL_INIT_EVERYTHING

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* args[])
 
 	SDL_Surface* screenSurface = NULL;
 
-	if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0)
+	if ( SDL_InitSubSystem( SDL_INIT_EVERYTHING ) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
 	}
@@ -23,7 +23,7 @@ int main(int argc, char* args[])
 								   SCREEN_WIDTH, 
 								   SCREEN_HEIGHT, 
 								   SDL_WINDOW_SHOWN);
-		if (window == NULL)
+		if ( window == NULL )
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 		}
@@ -32,8 +32,20 @@ int main(int argc, char* args[])
 			screenSurface = SDL_GetWindowSurface(window);
 			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 			SDL_UpdateWindowSurface(window);
-			SDL_Event e; bool quit = false; while (quit == false) {
-				while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; }
+			
+			SDL_Event event; 
+			bool running = true;
+			
+			while ( running ) 
+			{
+				while (SDL_PollEvent(&event)) 
+				{ 
+					if ( event.type == SDL_QUIT )
+					{
+						running = false;
+						break;
+					}
+				}
 			}
 		}
 	}
